36afcbbd2544304d0965a501386633775c7bf5e8
pgalak <panagiotis.tagkalakis@dialecticanet.com>
2022-10-21 19:12:15 +0300

    matrix: fix Cols and Rows funcs

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -1,51 +1,53 @@
 package matrix
 
 import (
-	"errors"
 	"strconv"
 	"strings"
 )
 
 // Define the Matrix type here.
 type Matrix struct {
-	rows [][]int
-	cols [][]int
+	data [][]int
 }
 
 func New(s string) (*Matrix, error) {
 	splitted := strings.Split(s, "\n")
-	var cols [][]int
-	var rows [][]int
+	var data [][]int
 
 	for i, r := range splitted {
 		rowInputs := strings.Split(r, " ")
-		rows = append(rows, []int{})
-		for _, rowInput := range rowInputs {
-			value, err := strconv.Atoi(rowInput)
-			if err != nil {
-				return nil, errors.New(err.Error())
+		data = append(data, []int{})
+		for _, char := range rowInputs {
+			if value, err := strconv.Atoi(strings.TrimLeft(string(char), " ")); err == nil {
+				data[i] = append(data[i], value)
 			}
-			rows[i] = append(rows[i], value)
 		}
 	}
 
-	for i := range rows {
-		for j := range rows[i] {
-			cols = append(cols, []int{})
-			cols[j] = append(cols[j], rows[i][j])
-		}
-	}
-
-	return &Matrix{rows: rows, cols: cols}, nil
+	return &Matrix{data: data}, nil
 }
 
 // Cols and Rows must return the results without affecting the matrix.
-func (m *Matrix) Cols() [][]int {
-	return append(make([][]int, 0, len(m.cols)), m.cols...)
+func (m *Matrix) Cols() (cols [][]int) {
+	cols = make([][]int, len(m.data[0]))
+	for j := 0; j < len(m.data); j++ {
+		for i := 0; i < len(cols); i++ {
+			cols[i] = append(cols[i], m.data[j][i])
+		}
+	}
+
+	return
 }
 
-func (m *Matrix) Rows() [][]int {
-	return append(make([][]int, 0, len(m.rows)), m.rows...)
+func (m *Matrix) Rows() (rows [][]int) {
+	rows = make([][]int, len(m.data))
+	for i := 0; i < len(rows); i++ {
+		for j := 0; j < len(m.data[i]); j++ {
+			rows[i] = append(rows[i], m.data[i][j])
+		}
+	}
+
+	return
 }
 
 func (m *Matrix) Set(row, col, val int) bool {
