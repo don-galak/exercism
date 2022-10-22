9ccffad6e10dbcef9d8994139bcea46c7801dbba
pgalak <panagiotis.tagkalakis@dialecticanet.com>
2022-10-22 11:24:38 +0300

    matrix: change Matrix type

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -7,50 +7,48 @@ import (
 )
 
 // Define the Matrix type here.
-type Matrix struct {
-	data [][]int
-}
+type Matrix [][]int
 
 func New(s string) (*Matrix, error) {
 	rows := strings.Split(s, "\n")
-	var data [][]int
+	var matrix Matrix
 
 	for i, row := range rows {
 		r := strings.Fields(row)
-		if i > 0 && len(r) != len(data[i-1]) {
+		if i > 0 && len(r) != len(matrix[i-1]) {
 			return nil, errors.New("matrix rows have inconsistent length")
 		}
 
-		data = append(data, []int{})
+		matrix = append(matrix, []int{})
 		for _, char := range r {
 			value, err := strconv.Atoi(char)
 			if err != nil {
 				return nil, err
 			}
-			data[i] = append(data[i], value)
+			matrix[i] = append(matrix[i], value)
 		}
 	}
 
-	return &Matrix{data: data}, nil
+	return &matrix, nil
 }
 
 // Cols and Rows must return the results without affecting the matrix.
-func (m *Matrix) Cols() (cols [][]int) {
-	cols = make([][]int, len(m.data[0]))
-	for j := 0; j < len(m.data); j++ {
+func (matrix *Matrix) Cols() (cols [][]int) {
+	cols = make([][]int, len((*matrix)[0]))
+	for j := 0; j < len(*matrix); j++ {
 		for i := 0; i < len(cols); i++ {
-			cols[i] = append(cols[i], m.data[j][i])
+			cols[i] = append(cols[i], (*matrix)[j][i])
 		}
 	}
 
 	return
 }
 
-func (m *Matrix) Rows() (rows [][]int) {
-	rows = make([][]int, len(m.data))
+func (matrix *Matrix) Rows() (rows [][]int) {
+	rows = make([][]int, len(*matrix))
 	for i := 0; i < len(rows); i++ {
-		for j := 0; j < len(m.data[i]); j++ {
-			rows[i] = append(rows[i], m.data[i][j])
+		for j := 0; j < len((*matrix)[i]); j++ {
+			rows[i] = append(rows[i], (*matrix)[i][j])
 		}
 	}
 
@@ -58,13 +56,10 @@ func (m *Matrix) Rows() (rows [][]int) {
 }
 
 func (m *Matrix) Set(row, col, val int) bool {
-	rowLen := len(m.data)
-	colLen := len(m.data[0])
-
-	if row < 0 || col < 0 || row >= rowLen || col >= colLen {
+	if row < 0 || col < 0 || row >= len(*m) || col >= len((*m)[0]) {
 		return false
 	}
 
-	m.data[row][col] = val
+	(*m)[row][col] = val
 	return true
 }
