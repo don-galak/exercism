3e4f10c53221a8196cbaec57eefe9de12b99aba4
pgalak <panagiotis.tagkalakis@dialecticanet.com>
2022-10-24 10:19:17 +0300

    bank_account: implement with RWMutex

--- bank-account/bank_account.go
+++ bank-account/bank_account.go
@@ -1,42 +1,33 @@
 package account
 
-import (
-	"sync"
-)
+import "sync"
 
 type Account struct {
 	balance int64
 	active  bool
-	mutex   sync.Mutex
+	mutex   sync.RWMutex
 }
 
 func Open(amount int64) *Account {
 	if amount < 0 {
 		return nil
 	}
-
-	acc := Account{balance: amount, active: true}
-	return &acc
+	return &Account{balance: amount, active: true}
 }
-
 func (a *Account) Balance() (int64, bool) {
-	if !a.active {
-		return 0, false
-	}
+	a.mutex.RLock()
+	defer a.mutex.RUnlock()
 	return a.balance, a.active
 }
-
 func (a *Account) Deposit(amount int64) (int64, bool) {
 	a.mutex.Lock()
 	defer a.mutex.Unlock()
 	if !a.active || a.balance+amount < 0 {
 		return 0, false
 	}
-
 	a.balance += amount
 	return a.balance, true
 }
-
 func (a *Account) Close() (int64, bool) {
 	a.mutex.Lock()
 	defer a.mutex.Unlock()
