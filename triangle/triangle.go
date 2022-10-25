2bc01985d9385cb4b3673566bea8469321d8e276
pgalak <panagiotis.tagkalakis@dialecticanet.com>
2022-10-25 09:33:25 +0300

    triangle: init and final

---
+++ triangle/triangle.go
@@ -0,0 +1,28 @@
+// This is a "stub" file.  It's a little start on your solution.
+// It's not a complete solution though; you have to write some code.
+
+// Package triangle should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package triangle
+
+type Kind string
+
+const (
+	Equ = "Equ"
+	Iso = "Iso"
+	Sca = "Sca"
+	NaT = "NaT"
+)
+
+func KindFromSides(a, b, c float64) Kind {
+	switch {
+	case a <= 0 || b <= 0 || c <= 0 || a+b < c || a+c < b || b+c < a:
+		return NaT
+	case a == b && b == c:
+		return Equ
+	case a == b || a == c || b == c:
+		return Iso
+	default:
+		return Sca
+	}
+}
