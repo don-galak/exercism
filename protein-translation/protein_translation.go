f1e6d1d5baf46aa02dd952978d99413655607d01
pgalak <panagiotis.tagkalakis@dialecticanet.com>
2022-11-07 09:22:03 +0200

    protein_translation: first solution

--- protein-translation/protein_translation.go
+++ protein-translation/protein_translation.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 )
 
-var ErrStop error = errors.New("error stop")
-var ErrInvalidBase error = errors.New("error invalid base")
+var ErrStop = errors.New("error stop")
+var ErrInvalidBase = errors.New("error invalid base")
 
 var codonMap = map[string]string{
 	"AUG": "Methionine",
@@ -19,43 +19,21 @@ var codonMap = map[string]string{
 	"UAA": "STOP", "UAG": "STOP", "UGA": "STOP",
 }
 
-// func tos(s int) string {
-// 	return strconv.Itoa(s)
-// }
-
-// func FromRNA(rna string) ([]string, error) {
-// 	proteinMap := map[string]string{}
-// 	protein := []string{}
-
-// 	for i := 0; i < len(rna); i += 3 {
-// 		codon := fmt.Sprintf("%v%v%v", string(rna[i]), string(rna[i+1]), string(rna[i+2]))
-// 		// println(rna[i], reflect.TypeOf(rna[i]), rna[i+1], rna[i+2], codon)
-// 		if _, exists := codonMap[codon]; !exists {
-// 			return nil, ErrInvalidBase
-// 		}
-// 		if _, exists := proteinMap[codonMap[codon]]; !exists {
-// 			proteinMap[codonMap[codon]] = codon
-// 			protein = append(protein, codonMap[codon])
-// 		}
-// 	}
-// 	return protein, nil
-// }
-
 func FromRNA(rna string) ([]string, error) {
-	proteinMap := map[string]string{}
+	proteinMap := map[string]int{}
 	protein := []string{}
 
 	for i := 0; i < len(rna); i += 3 {
 		codon := fmt.Sprintf("%v%v%v", string(rna[i]), string(rna[i+1]), string(rna[i+2]))
-		println(codon)
-		if _, err := FromCodon(codonMap[codon]); err == ErrInvalidBase {
-			return nil, err
+
+		if _, err := FromCodon(codon); err == ErrInvalidBase {
+			return nil, ErrInvalidBase
 		} else if err == ErrStop {
 			break
 		}
 
 		if _, exists := proteinMap[codonMap[codon]]; !exists {
-			proteinMap[codonMap[codon]] = codon
+			proteinMap[codonMap[codon]] = 1
 			protein = append(protein, codonMap[codon])
 		}
 	}
