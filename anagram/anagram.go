1344ed4e74dd19be6684d6de3bdd5bd82cc12680
pgalak <panagiotis.tagkalakis@dialecticanet.com>
2022-11-11 10:14:23 +0200

    anagram: added extra info

--- anagram/anagram.go
+++ anagram/anagram.go
@@ -6,6 +6,8 @@ import "strings"
 // will it work for other alphabets?
 var primes = [26]uint{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101}
 
+// * INFO: https://en.wikipedia.org/wiki/Fundamental_theorem_of_arithmetic
+
 // TODO - Collisions are possible with unicode strings
 func hashString(in string) uint {
 	var hash uint = 1
