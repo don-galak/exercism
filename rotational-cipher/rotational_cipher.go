52ad78d7709d11946ae0535e40a2f01b4d02d61d
don-galak <ptagalac@gmail.com>
2022-11-20 22:01:20 +0200

    rotational-cipher: init and first draft

---
+++ rotational-cipher/rotational_cipher.go
@@ -0,0 +1,37 @@
+package rotationalcipher
+
+import (
+	"strings"
+)
+
+const (
+	a = 97
+	z = 122
+	Z = 90
+	A = 65
+)
+
+func RotationalCipher(plain string, shiftKey int) string {
+	var w strings.Builder
+	if shiftKey == 26 {
+		shiftKey = 0
+	}
+
+	for _, r := range plain {
+		shifted := r + rune(shiftKey)
+		if r >= a && r <= z {
+			if shifted > z {
+				shifted = a + (z - r)
+			}
+		} else if r >= A && r <= Z {
+			if shifted > Z {
+				shifted = A + (Z - r)
+			}
+		} else {
+			shifted = r
+		}
+		w.WriteRune(shifted)
+	}
+
+	return w.String()
+}
