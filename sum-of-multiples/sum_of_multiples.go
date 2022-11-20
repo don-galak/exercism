56108e423ac37a5542c4ea41eb8c2601f3c13585
don-galak <ptagalac@gmail.com>
2022-11-20 19:13:13 +0200

    sum-of-multiples: optimize

--- sum-of-multiples/sum_of_multiples.go
+++ sum-of-multiples/sum_of_multiples.go
@@ -3,10 +3,7 @@ package summultiples
 func SumMultiples(limit int, divisors ...int) (sum int) {
 	for i := 1; i < limit; i++ {
 		for _, divisor := range divisors {
-			if divisor == 0 {
-				continue
-			}
-			if i%divisor == 0 {
+			if divisor != 0 && i%divisor == 0 {
 				sum += i
 				break
 			}
