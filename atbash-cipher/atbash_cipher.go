496910345b2177403ad9822c15b2d87b7f6da125
don-galak <ptagalac@gmail.com>
2022-11-21 22:56:50 +0200

    atbash-cipher: optimize

--- atbash-cipher/atbash_cipher.go
+++ atbash-cipher/atbash_cipher.go
@@ -1,36 +1,21 @@
 package atbash
 
 import (
-	"regexp"
 	"strings"
 )
 
-const (
-	zero = 48
-	nine = 57
-	a    = 97
-	z    = 122
-	Z    = 90
-	A    = 65
-)
-
-var re = regexp.MustCompile(`[^a-zA-Z0-9]+`)
-
 func Atbash(s string) string {
 	var w strings.Builder
-	s = re.ReplaceAllString(s, "")
-	for i, r := range s {
-		if i != 0 && i%5 == 0 {
+	for _, r := range strings.ToLower(s) {
+		if (w.Len()+1)%6 == 0 {
 			w.WriteRune(' ')
 		}
 		switch {
-		case r >= zero && r <= nine:
+		case r >= '0' && r <= '9':
 			w.WriteRune(r)
-		case r >= a && r <= z:
-			w.WriteRune(z - (r - a))
-		case r >= A && r <= Z:
-			w.WriteRune(Z - (r - A) + 32)
+		case r >= 'a' && r <= 'z':
+			w.WriteRune('z' - (r - 'a'))
 		}
 	}
-	return w.String()
+	return strings.TrimRight(w.String(), " ")
 }
