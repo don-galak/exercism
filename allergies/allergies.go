f6f4f02ae79945db8c8eb7e23f33ec809f9595df
don-galak <ptagalac@gmail.com>
2023-01-24 20:31:58 +0200

    allergies: improve

--- allergies/allergies.go
+++ allergies/allergies.go
@@ -1,49 +1,34 @@
 package allergies
 
-const (
-	eggs         uint = 1
-	peanuts      uint = 2
-	shellfish    uint = 4
-	strawberries uint = 8
-	tomatoes     uint = 16
-	chocolate    uint = 32
-	pollen       uint = 64
-	cats         uint = 128
-	maxAllergies uint = 256
-)
+var allergiesNames = []string{
+	"cats",
+	"pollen",
+	"chocolate",
+	"tomatoes",
+	"strawberries",
+	"shellfish",
+	"peanuts",
+	"eggs",
+}
+
+const maxAllergies = 256
 
 func Allergies(allergies uint) (out []string) {
+	var maxAllergyValue uint = 128
+
 	if allergies > maxAllergies {
 		allergies = allergies % maxAllergies
 	}
 
+	i := 0
+
 	for allergies > 0 {
-		switch {
-		case allergies >= cats:
-			allergies -= cats
-			out = append(out, "cats")
-		case allergies >= pollen:
-			allergies -= pollen
-			out = append(out, "pollen")
-		case allergies >= chocolate:
-			allergies -= chocolate
-			out = append(out, "chocolate")
-		case allergies >= tomatoes:
-			allergies -= tomatoes
-			out = append(out, "tomatoes")
-		case allergies >= strawberries:
-			allergies -= strawberries
-			out = append(out, "strawberries")
-		case allergies >= shellfish:
-			allergies -= shellfish
-			out = append(out, "shellfish")
-		case allergies >= peanuts:
-			allergies -= peanuts
-			out = append(out, "peanuts")
-		case allergies >= eggs:
-			allergies -= eggs
-			out = append(out, "eggs")
+		if allergies >= maxAllergyValue {
+			allergies -= maxAllergyValue
+			out = append(out, allergiesNames[i])
 		}
+		i++
+		maxAllergyValue /= 2
 	}
 
 	return
