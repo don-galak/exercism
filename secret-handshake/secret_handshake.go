e093c660194544765688a7a048788319646b6d76
don-galak <ptagalac@gmail.com>
2023-01-26 23:11:00 +0200

    secret-handshake: final solution

--- secret-handshake/secret_handshake.go
+++ secret-handshake/secret_handshake.go
@@ -1,35 +1,30 @@
 package secret
 
+const (
+	Wink = 1 << iota
+	DoubleBlink
+	CloseEyes
+	Jump
+	Reverse
+)
+
 func Handshake(code uint) (out []string) {
-	reverseTrigger := false
-
-	for code > 0 {
-		switch {
-		case code >= 16:
-			code -= 16
-			reverseTrigger = true
-		case code >= 8:
-			code -= 8
-			out = append(out, "jump")
-		case code >= 4:
-			code -= 4
-			out = append(out, "close your eyes")
-		case code >= 2:
-			code -= 2
-			out = append(out, "double blink")
-		case code >= 1:
-			code -= 1
-			out = append(out, "wink")
-		}
+	if code&Wink != 0 {
+		out = append(out, "wink")
 	}
-
-	// actions are added in the slice, in a reverse facion, so reversing needs to happen
-	// only when it's actually not needed
-	if !reverseTrigger {
+	if code&DoubleBlink != 0 {
+		out = append(out, "double blink")
+	}
+	if code&CloseEyes != 0 {
+		out = append(out, "close your eyes")
+	}
+	if code&Jump != 0 {
+		out = append(out, "jump")
+	}
+	if code&Reverse != 0 {
 		for i, j := 0, len(out)-1; i < j; i, j = i+1, j-1 {
 			out[i], out[j] = out[j], out[i]
 		}
 	}
-
 	return
 }
