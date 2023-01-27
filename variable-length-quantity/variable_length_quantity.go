e07a2c88f6067445faebf0cf505a6da911724c88
don-galak <ptagalac@gmail.com>
2023-01-27 19:54:44 +0200

    variable_length_quantity: final

--- variable-length-quantity/variable_length_quantity.go
+++ variable-length-quantity/variable_length_quantity.go
@@ -1,9 +1,42 @@
 package variablelengthquantity
 
-func EncodeVarint(input []uint32) []byte {
-	panic("Please implement the EncodeVarint function")
+import "errors"
+
+func reverse(in []byte) {
+	for i, j := 0, len(in)-1; i < j; i, j = i+1, j-1 {
+		in[i], in[j] = in[j], in[i]
+	}
+}
+
+func EncodeVarint(input []uint32) (out []byte) {
+	for _, el := range input {
+		extracted := []byte{byte(el % 128)}
+		for el >>= 7; el != 0; el >>= 7 {
+			println(el, el>>7)
+			extracted = append(extracted, 128+byte(el%128))
+		}
+		reverse(extracted)
+		out = append(out, extracted...)
+	}
+	return nil
 }
 
 func DecodeVarint(input []byte) ([]uint32, error) {
-	panic("Please implement the DecodeVarint function")
+	var d uint32
+	var complete bool
+	decodedInts := make([]uint32, 0)
+	for _, b := range input {
+		d += uint32(b & 0x7F)
+		complete = (b&0x80 == 0)
+		if complete {
+			decodedInts = append(decodedInts, d)
+			d = 0
+			continue
+		}
+		d <<= 7
+	}
+	if !complete {
+		return nil, errors.New("incomplete sequence")
+	}
+	return decodedInts, nil
 }
--- variable-length-quantity/variable_length_quantity_test.go
+++ variable-length-quantity/variable_length_quantity_test.go
@@ -2,6 +2,7 @@ package variablelengthquantity
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
@@ -15,20 +16,20 @@ func TestEncodeVarint(t *testing.T) {
 	}
 }
 
-// func TestDecodeVarint(t *testing.T) {
-// 	for _, tc := range decodeTestCases {
-// 		t.Run(tc.description, func(t *testing.T) {
-// 			actual, err := DecodeVarint(tc.input)
-// 			switch {
-// 			case tc.errorExpected:
-// 				if err == nil {
-// 					t.Fatalf("DecodeVarint(%#v) expected error, got: %#v", tc.input, actual)
-// 				}
-// 			case err != nil:
-// 				t.Fatalf("DecodeVarint(%#v) returned error: %v, want:%#v", tc.input, err, tc.expected)
-// 			case !reflect.DeepEqual(actual, tc.expected):
-// 				t.Fatalf("DecodeVarint(%#v) = %#v, want:%#v", tc.input, actual, tc.expected)
-// 			}
-// 		})
-// 	}
-// }
+func TestDecodeVarint(t *testing.T) {
+	for _, tc := range decodeTestCases {
+		t.Run(tc.description, func(t *testing.T) {
+			actual, err := DecodeVarint(tc.input)
+			switch {
+			case tc.errorExpected:
+				if err == nil {
+					t.Fatalf("DecodeVarint(%#v) expected error, got: %#v", tc.input, actual)
+				}
+			case err != nil:
+				t.Fatalf("DecodeVarint(%#v) returned error: %v, want:%#v", tc.input, err, tc.expected)
+			case !reflect.DeepEqual(actual, tc.expected):
+				t.Fatalf("DecodeVarint(%#v) = %#v, want:%#v", tc.input, actual, tc.expected)
+			}
+		})
+	}
+}
