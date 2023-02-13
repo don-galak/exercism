3f137d21e2aa9bd744bd836417c907af089a036e
don-galak <ptagalac@gmail.com>
2023-02-13 22:16:17 +0200

    rail_fence_cipher: add first draft for Decode

--- rail-fence-cipher/rail_fence_cipher.go
+++ rail-fence-cipher/rail_fence_cipher.go
@@ -37,5 +37,45 @@ func Encode(message string, rails int) string {
 }
 
 func Decode(message string, rails int) string {
-	return ""
+	var encoded bytes.Buffer
+	msgCopy := message
+	arr := make([][]rune, rails)
+	for i := 0; i < rails; i++ {
+		arr[i] = make([]rune, len(message))
+	}
+
+	add := true
+	j := 0
+	for i := range message {
+		if j == rails-1 {
+			add = false
+		} else if j == 0 {
+			add = true
+		}
+		arr[j][i] = 1
+		if add {
+			j++
+		} else {
+			j--
+		}
+	}
+
+	for i := range arr {
+		for j, char := range arr[i] {
+			if char == 1 {
+				arr[i][j] = rune(msgCopy[0])
+				msgCopy = msgCopy[1:]
+			}
+		}
+	}
+
+	for i := range message {
+		for j := range arr {
+			if arr[j][i] != 0 {
+				encoded.WriteRune(arr[j][i])
+			}
+		}
+	}
+
+	return encoded.String()
 }
