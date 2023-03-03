e83c3a480d24adfb7ea19f3f25b1820f00e417b9
don-galak <ptagalac@gmail.com>
2023-03-03 09:18:57 +0200

    pov: final

--- pov/pov.go
+++ pov/pov.go
@@ -2,30 +2,24 @@ package pov
 
 type Tree struct {
 	value    string
-	parent   *Tree
 	children []*Tree
-	// Add the needed fields here
 }
 
-// New creates and returns a new Tree with the given root value and children.
-func New(value string, children ...*Tree) *Tree {
-	root := &Tree{value: value, children: children}
-	for _, ch := range children {
-		ch.parent = root
+func New(value string, children ...*Tree) *Tree { return &Tree{value: value, children: children} }
+
+func (tr *Tree) Value() string     { return tr.value }
+func (tr *Tree) Children() []*Tree { return tr.children }
+func (tr *Tree) createPath(from string, path []*Tree) []*Tree {
+	path = append(path, tr)
+	if tr.value == from {
+		return path
 	}
-	return root
-}
-
-// Value returns the value at the root of a tree.
-func (tr *Tree) Value() string {
-	return tr.value
-}
-
-// Children returns a slice containing the children of a tree.
-// There is no need to sort the elements in the result slice,
-// they can be in any order.
-func (tr *Tree) Children() []*Tree {
-	return tr.children
+	for _, ch := range tr.children {
+		if p := ch.createPath(from, path); p != nil {
+			return p
+		}
+	}
+	return nil
 }
 
 // String describes a tree in a compact S-expression format.
@@ -46,52 +40,38 @@ func (tr *Tree) String() string {
 }
 
 // POV problem-specific functions
-func findNode(node *Tree, from string) *Tree {
-	if node.value == from {
-		return node
-	}
-	for _, ch := range node.children {
-		l := findNode(ch, from)
-		if l != nil && l.value == from {
-			return l
-		}
-	}
-	return nil
-}
-
 // FromPov returns the pov from the node specified in the argument.
 func (tr *Tree) FromPov(from string) *Tree {
-	if tr.value == from {
-		return tr
-	}
-	node := findNode(tr, from)
-	if node == nil {
+	path := tr.createPath(from, make([]*Tree, 0))
+	pathLength := len(path)
+	if pathLength == 0 {
 		return nil
 	}
 
-	parent := node.parent
-	var index int
-	for i, ch := range parent.children {
-		if ch.value == from {
-			index = i
-			break
+	for i := pathLength - 1; i > 0; i-- {
+		child, parent := path[i], path[i-1]
+		for j, ch := range parent.children {
+			if ch == child {
+				parent.children = append(parent.children[:j], parent.children[j+1:]...)
+				break
+			}
 		}
+		child.children = append(child.children, parent)
 	}
-
-	parent.children[index] = parent.children[len(parent.children)-1]
-	parent.children = parent.children[:len(parent.children)-1]
-	if parent.parent != nil {
-		println("PARENT PARENT: ", parent.parent.String())
-		// parent.children = []*Tree{parent.parent}
-	}
-	parent.parent = node
-	node.parent = nil
-	node.children = append(node.children, parent)
-
-	return node
+	return path[len(path)-1]
 }
 
 // PathTo returns the shortest path between two nodes in the tree.
 func (tr *Tree) PathTo(from, to string) []string {
-	panic("Please implement this function")
+	fromTree := tr.FromPov(from)
+	if fromTree == nil {
+		return nil
+	}
+
+	path := fromTree.createPath(to, make([]*Tree, 0))
+	p := make([]string, 0, len(path))
+	for _, node := range path {
+		p = append(p, node.value)
+	}
+	return p
 }
