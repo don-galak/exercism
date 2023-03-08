83f8cd6cf15c87f91352d9d4e69dc46274fcd47c
don-galak <ptagalac@gmail.com>
2023-03-08 21:50:20 +0200

    resistor_color_trio: improved solution

--- resistor-color-trio/resistor_color_trio.go
+++ resistor-color-trio/resistor_color_trio.go
@@ -5,14 +5,8 @@ import (
 	"math"
 )
 
-const (
-	ohms, kilo, mega, giga = "ohms", "kilo", "mega", "giga"
-)
-const (
-	KO, MO, GO = 1000, KO * KO, MO * KO
-)
-
 var colorSlice = []string{"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"}
+var magnitudes = []string{"", "kilo", "mega", "giga"}
 
 func findColorIndex(color string) int {
 	for i, c := range colorSlice {
@@ -23,34 +17,15 @@ func findColorIndex(color string) int {
 	return -1
 }
 
-func Value(colors []string) string {
-	zeroes := findColorIndex(colors[2])
-	res := math.Pow(10, float64(zeroes))
-	final := (findColorIndex(colors[0])*10 + findColorIndex(colors[1])) * int(res)
-
-	if final == 0 {
-		return "0 ohms"
-	}
-
-	var suffix string
-	switch {
-	case final%GO == 0:
-		suffix = giga
-		final /= GO
-	case final%MO == 0:
-		suffix = mega
-		final /= MO
-	case final%KO == 0:
-		suffix = kilo
-		final /= KO
-	default:
-		suffix = ""
-	}
-	suffix += ohms
-
-	return fmt.Sprintf("%d %v", final, suffix)
-}
-
 func Label(colors []string) string {
-	return Value(colors)
+	c1 := findColorIndex(colors[0])
+	c2 := findColorIndex(colors[1])
+	zeroes := math.Pow10(findColorIndex(colors[2]))
+	resistance := (c1*10 + c2) * int(zeroes)
+	mag := 0
+
+	for ; resistance >= 1000; resistance /= 1000 {
+		mag++
+	}
+	return fmt.Sprintf("%d %sohms", resistance, magnitudes[mag])
 }
