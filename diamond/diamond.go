94fd8238e58d67868a1b8229dfff39f2a6661c24
don-galak <ptagalac@gmail.com>
2023-03-20 23:59:14 +0200

    diamond: improve variable names

--- diamond/diamond.go
+++ diamond/diamond.go
@@ -12,27 +12,27 @@ func Gen(char byte) (string, error) {
 		return "", errOutOfRange
 	}
 
-	start := 'A'
-	sliceLen := int(char) - 64
-	slice := make([]string, sliceLen)
-	sliceLen--
+	letter := 'A'
+	spacesAround := int(char) - 64
+	slice := make([]string, spacesAround)
+	spacesAround--
 
-	slice[0] = strings.Repeat(" ", sliceLen)
-	slice[0] += string(start)
-	slice[0] += strings.Repeat(" ", sliceLen)
+	slice[0] = strings.Repeat(" ", spacesAround)
+	slice[0] += string(letter)
+	slice[0] += strings.Repeat(" ", spacesAround)
 
-	between := 1
-	sliceLen--
-	start++
+	spacesBetween := 1
+	spacesAround--
+	letter++
 	for i := 1; i < len(slice); i++ {
-		slice[i] = strings.Repeat(" ", sliceLen)
-		slice[i] += string(start)
-		slice[i] += strings.Repeat(" ", between)
-		slice[i] += string(start)
-		slice[i] += strings.Repeat(" ", sliceLen)
-		between += 2
-		sliceLen--
-		start++
+		slice[i] = strings.Repeat(" ", spacesAround)
+		slice[i] += string(letter)
+		slice[i] += strings.Repeat(" ", spacesBetween)
+		slice[i] += string(letter)
+		slice[i] += strings.Repeat(" ", spacesAround)
+		spacesBetween += 2
+		spacesAround--
+		letter++
 	}
 
 	for i := len(slice) - 2; i > -1; i-- {
