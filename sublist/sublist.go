e70109b441c5688da930c843ca7296fa2a33d363
don-galak <ptagalac@gmail.com>
2023-03-23 21:54:17 +0200

    sublist: improve

--- sublist/sublist.go
+++ sublist/sublist.go
@@ -2,28 +2,28 @@ package sublist
 
 import (
 	"bytes"
-	"fmt"
+	"strconv"
 	"strings"
 )
 
-func toString(l []int) string {
+func write(l []int) string {
 	var s bytes.Buffer
 	for _, x := range l {
-		s.WriteString(fmt.Sprint(x, "."))
+		s.WriteString(strconv.Itoa(x) + ".")
 	}
 	return s.String()
 }
 
 func Sublist(l1, l2 []int) Relation {
-	ls1 := toString(l1)
-	ls2 := toString(l2)
+	s1 := write(l1)
+	s2 := write(l2)
 
 	switch {
-	case ls1 == ls2:
+	case s1 == s2:
 		return RelationEqual
-	case strings.Contains(ls2, ls1):
+	case strings.Contains(s2, s1):
 		return RelationSublist
-	case strings.Contains(ls1, ls2):
+	case strings.Contains(s1, s2):
 		return RelationSuperlist
 	default:
 		return RelationUnequal
