d8fb793dbd0b41f7aa6244cbda7d895371a9d6a1
don-galak <ptagalac@gmail.com>
2023-03-29 23:10:38 +0300

    grep: first draft

--- grep/grep.go
+++ grep/grep.go
@@ -1,9 +1,54 @@
 package grep
 
-import "fmt"
+import (
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+// var flagMap = map[string]bool{"-n": true, "-l": true, "-i":true, "-v":true}
 
 func Search(pattern string, flags, files []string) []string {
-	fmt.Printf("pattern: %s\nflags %s\nfiles: %v\n\n", pattern, flags, files)
+	result := make([]string, 0)
+	flagMap := make(map[string]bool, len(flags))
+	for _, f := range flags {
+		flagMap[f] = true
+	}
 
-	return []string{}
+	// fmt.Printf("pattern: %s\nflags %s\nfiles: %v\n", pattern, flags, files)
+	fileContents := make(map[string]string, len(files))
+	for _, file := range files {
+		if fc, ok := os.ReadFile(file); ok == nil {
+			fileContents[file] = string(fc)
+		}
+	}
+
+	for file, fc := range fileContents {
+		for i, line := range strings.Split(fc, "\n") {
+			if flagMap["-i"] {
+				r, _ := regexp.Compile("(?i)" + pattern)
+				if r.MatchString(line) {
+					result = append(result, line)
+				}
+			}
+
+			if strings.Contains(line, pattern) {
+				switch {
+				case flagMap["-n"]:
+					line = strconv.Itoa(i+1) + ":" + line
+				case flagMap["-l"]:
+					line = file
+					// case flagMap["-x"]:
+
+				}
+
+				result = append(result, line)
+			}
+		}
+	}
+
+	// fmt.Printf("files contents: \n%s\n\n", fileContents)
+
+	return result
 }
