c53ebb21cb9459b6a585b8192c67a49501d5c625
don-galak <ptagalac@gmail.com>
2023-03-31 11:54:10 +0300

    grep: handle pattern matching flags besides invert

--- grep/grep.go
+++ grep/grep.go
@@ -7,16 +7,27 @@ import (
 	"strings"
 )
 
-// var flagMap = map[string]bool{"-n": true, "-l": true, "-i":true, "-v":true}
+func addToResult(results *[]string, line string) {
+	for _, r := range *results {
+		if r == line {
+			return
+		}
+	}
+	*results = append(*results, line)
+}
 
 func Search(pattern string, flags, files []string) []string {
-	resultMap := make(map[string]bool)
+	result := make([]string, 0)
 	flagMap := make(map[string]bool, len(flags))
 	for _, f := range flags {
 		flagMap[f] = true
 	}
+	caseInsensitiveMatchWholeLine := flagMap["-i"] && flagMap["-x"]
+	matchWholeLine := !flagMap["-i"] && flagMap["-x"]
+	caseInsensitive := flagMap["-i"] && !flagMap["-x"]
+	defaultMatch := !flagMap["-i"] && !flagMap["-x"]
+	inverse := flagMap["-v"]
 
-	// fmt.Printf("pattern: %s\nflags %s\nfiles: %v\n", pattern, flags, files)
 	fileContents := make(map[string]string, len(files))
 	for _, file := range files {
 		if fc, ok := os.ReadFile(file); ok == nil {
@@ -26,13 +37,15 @@ func Search(pattern string, flags, files []string) []string {
 
 	for file, fc := range fileContents {
 		for i, line := range strings.Split(fc, "\n") {
-			if flagMap["-i"] {
-				pattern = "(?i)" + pattern
+			matches := false
+
+			if (caseInsensitiveMatchWholeLine && strings.EqualFold(pattern, line)) || matchWholeLine && pattern == line || defaultMatch && strings.Contains(line, pattern) {
+				matches = true
+			} else if r, _ := regexp.Compile("(?i)" + pattern); r.MatchString(line) && caseInsensitive {
+				matches = true
 			}
 
-			r, _ := regexp.Compile(pattern)
-
-			if r.MatchString(line) {
+			if matches && !inverse {
 				switch {
 				case flagMap["-l"]:
 					line = file
@@ -43,18 +56,9 @@ func Search(pattern string, flags, files []string) []string {
 					line = file + ":" + line
 				}
 
-				// ! this wont output the correct result at all times
-				// ! later when we append the keys in a slice, the result might not sorted
-				// ! and some tests will fail
-				resultMap[line] = true
+				addToResult(&result, line)
 			}
 		}
-	}
-	//
-	// fmt.Printf("files contents: \n%s\n\n", fileContents)
-	result := make([]string, 0)
-	for key := range resultMap {
-		result = append(result, key)
 	}
 
 	return result
