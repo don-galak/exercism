d92162f803b2bf02d9b3cb756d3b44818382c003
don-galak <ptagalac@gmail.com>
2023-04-02 23:18:45 +0300

    pascals-triangle: improve(kinda)

--- pascals-triangle/pascals_triangle.go
+++ pascals-triangle/pascals_triangle.go
@@ -1,25 +1,17 @@
 package pascal
 
 func Triangle(n int) [][]int {
-	firstRow := []int{1}
 	triangle := make([][]int, n)
-	triangle[0] = firstRow
+	triangle[0] = []int{1}
 
 	for i := 1; i < n; i++ {
-		row := make([]int, i+1)
+		triangle[i] = make([]int, i+1)
+		triangle[i][0] = 1
+		triangle[i][i] = 1
 
-		for j := 0; j < i+1; j++ {
-			prevRow := triangle[i-1]
-
-			if j-1 < 0 {
-				row[j] = prevRow[j]
-			} else if len(prevRow) == j {
-				row[j] = prevRow[j-1]
-			} else {
-				row[j] = prevRow[j-1] + prevRow[j]
-			}
+		for j := 1; j < i; j++ {
+			triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
 		}
-		triangle[i] = row
 	}
 
 	return triangle
