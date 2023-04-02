b4a9d578f44fd6ce04ad428f90948208f5c2acf8
don-galak <ptagalac@gmail.com>
2023-04-02 21:43:36 +0300

    grep: replace os.ReadFile implementation with os.Open

--- grep/grep.go
+++ grep/grep.go
@@ -1,65 +1,74 @@
 package grep
 
 import (
+	"bufio"
+	"fmt"
+	"log"
 	"os"
 	"regexp"
 	"strconv"
-	"strings"
 )
 
-func addToResult(results *[]string, line string) {
-	for _, r := range *results {
-		if r == line {
-			return
+func hasValue(values []string, value string) bool {
+	for _, v := range values {
+		if v == value {
+			return true
 		}
 	}
-	*results = append(*results, line)
+	return false
+}
+
+func compileRegexp(pattern string, flags []string) (*regexp.Regexp, error) {
+	matchWholeLine := hasValue(flags, "-x")
+	if matchWholeLine {
+		pattern = fmt.Sprintf("^%s$", pattern)
+	}
+	caseInsensitive := hasValue(flags, "-i")
+	if caseInsensitive {
+		pattern = "(?i)" + pattern
+	}
+	return regexp.Compile(pattern)
 }
 
 func Search(pattern string, flags, files []string) []string {
 	result := make([]string, 0)
-	flagMap := make(map[string]bool, len(flags))
-	for _, f := range flags {
-		flagMap[f] = true
+	withMultipleFiles := len(files) > 1
+	withLineNumbers := hasValue(flags, "-n")
+	withFileNames := hasValue(flags, "-l")
+	invert := hasValue(flags, "-v")
+	exp, e := compileRegexp(pattern, flags)
+	if e != nil {
+		log.Fatal(e)
+		return nil
 	}
-	caseInsensitiveMatchWholeLine := flagMap["-i"] && flagMap["-x"]
-	matchWholeLine := !flagMap["-i"] && flagMap["-x"]
-	caseInsensitive := flagMap["-i"] && !flagMap["-x"]
-	defaultMatch := !flagMap["-i"] && !flagMap["-x"]
-	inverse := flagMap["-v"]
 
-	fileContents := make(map[string]string, len(files))
 	for _, file := range files {
-		if fc, ok := os.ReadFile(file); ok == nil {
-			fileContents[file] = string(fc)
+		f, e := os.Open(file)
+		if e != nil {
+			log.Fatal(e)
+			return nil
 		}
-	}
+		defer f.Close()
 
-	for file, fc := range fileContents {
-		for i, line := range strings.Split(fc, "\n") {
-			matches := false
-
-			if (caseInsensitiveMatchWholeLine && strings.EqualFold(pattern, line)) || matchWholeLine && pattern == line || defaultMatch && strings.Contains(line, pattern) {
-				matches = true
-			} else if r, _ := regexp.Compile("(?i)" + pattern); r.MatchString(line) && caseInsensitive {
-				matches = true
-			}
-
-			if matches && !inverse {
-				switch {
-				case flagMap["-l"]:
-					line = file
-				case !flagMap["-l"] && flagMap["-n"]:
-					line = strconv.Itoa(i+1) + ":" + line
+		reader := bufio.NewReader(f)
+		lineNum := 0
+		for b, _, e := reader.ReadLine(); e == nil; b, _, e = reader.ReadLine() {
+			line := string(b)
+			lineNum++
+			if exp.MatchString(line) != invert {
+				if withLineNumbers {
+					line = strconv.Itoa(lineNum) + ":" + line
 				}
-				if len(files) > 1 && !flagMap["-l"] {
+				if withMultipleFiles && !withFileNames {
 					line = file + ":" + line
 				}
-
-				addToResult(&result, line)
+				if withFileNames && !hasValue(result, file) {
+					result = append(result, file)
+				} else if !withFileNames {
+					result = append(result, line)
+				}
 			}
 		}
 	}
-
 	return result
 }
