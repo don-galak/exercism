3db024aa8a991ee22237a41342d9c1b1b8f34d87
don-galak <ptagalac@gmail.com>
2023-04-18 23:44:40 +0300

    circular-buffer: first draft

--- circular-buffer/circular_buffer.go
+++ circular-buffer/circular_buffer.go
@@ -2,48 +2,36 @@ package circular
 
 import (
 	"errors"
-	"io"
 )
 
-const opRead readOp = -1
-const opInvalid readOp = 0
+var errBufferFull = errors.New("buffer is full")
+var errBufferEmpty = errors.New("buffer is empty")
 
-var ErrTooLarge = errors.New("bytes.Buffer: too large")
-
-type readOp int8
 type Buffer struct {
-	buf      []byte
-	off      int
-	lastRead readOp
-	size     int
+	buf  []byte
+	size int
 }
 
 func NewBuffer(size int) *Buffer {
 	return &Buffer{size: size}
 }
 
-func (b *Buffer) empty() bool { return len(b.buf) <= b.off }
-
 func (b *Buffer) ReadByte() (byte, error) {
-	if b.empty() {
+	if len(b.buf) == 0 {
 		b.Reset()
-		return 0, io.EOF
+		return 0, errBufferEmpty
 	}
-	c := b.buf[b.off]
-	b.off++
-	b.lastRead = opRead
+	c := b.buf[0]
+	b.buf = b.buf[1:]
 	return c, nil
 }
 
-func (b *Buffer) Len() int { return len(b.buf) - b.off }
-
 func (b *Buffer) WriteByte(c byte) error {
 	if len(b.buf) < b.size {
-		println(string(c))
 		b.buf = append(b.buf, c)
 		return nil
 	}
-	return ErrTooLarge
+	return errBufferFull
 }
 
 func (b *Buffer) Overwrite(c byte) {
@@ -54,6 +42,4 @@ func (b *Buffer) Overwrite(c byte) {
 
 func (b *Buffer) Reset() {
 	b.buf = b.buf[:0]
-	b.off = 0
-	b.lastRead = opInvalid
 }
