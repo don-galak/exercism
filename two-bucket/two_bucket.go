67c3aa339d30b30a65516fa79e2884b9c983fd47
don-galak <ptagalac@gmail.com>
2023-04-29 15:28:46 +0300

    two-bucket: final

--- two-bucket/two_bucket.go
+++ two-bucket/two_bucket.go
@@ -1,112 +1,56 @@
 package twobucket
 
-import (
-	"errors"
-	"fmt"
-)
+import "errors"
 
-const (
-	one = "one"
-	two = "two"
-)
+type bucket struct{ name, size, level int }
 
-var errInvalidInput = errors.New("invalid")
-var invalid = func() (string, int, int, error) { return "", 0, 0, errInvalidInput }
+var names = map[int]string{1: "one", 2: "two"}
 
-func Solve(sizeBucketOne, sizeBucketTwo, goalAmount int, startBucket string) (string, int, int, error) {
-	if sizeBucketOne == 0 || sizeBucketTwo == 0 || goalAmount == 0 {
-		return invalid()
+func Solve(thisSize, otherSize, goal int, start string) (string, int, int, error) {
+	if err := validateInput(thisSize, otherSize, goal, start); err != nil {
+		return "", 0, 0, err
 	}
 
+	this, other := &bucket{1, thisSize, 0}, &bucket{2, otherSize, 0}
+	if start == names[2] {
+		this, other = other, this
+	}
 	moves := 0
-	bucketOne := 0
-	bucketTwo := 0
-
-	if startBucket == one {
-		//fill bucket one
-		bucketOne = sizeBucketOne
-		moves++
-
-		//pour bucket one in bucket two
-		for bucketOne > 0 {
-			bucketOne--
-			bucketTwo++
+	for ; this.level != goal && other.level != goal; moves++ {
+		switch {
+		case this.level == 0:
+			this.level = this.size
+		case other.size == goal:
+			other.level = goal
+		case other.level == other.size:
+			other.level = 0
+		default:
+			pour := other.size - other.level
+			if this.level < pour {
+				pour = this.level
+			}
+			this.level -= pour
+			other.level += pour
 		}
-		moves++
-
-		// fill  bucket one
-		bucketOne = sizeBucketOne
-		moves++
-
-		//pour bucket one in bucket two
-		for bucketTwo < sizeBucketTwo {
-			bucketOne--
-			bucketTwo++
-		}
-		moves++
-
-		return one, moves, bucketTwo, nil
 	}
-
-	if startBucket == two {
-		//fill bucket two
-		bucketTwo = sizeBucketTwo
-		moves++
-		printer(moves, bucketOne, bucketTwo)
-
-		//pour bucket two in bucket one
-		for bucketOne < sizeBucketOne {
-			bucketTwo--
-			bucketOne++
-		}
-		moves++
-		printer(moves, bucketOne, bucketTwo)
-
-		// empty bucket one
-		bucketOne = 0
-		moves++
-		printer(moves, bucketOne, bucketTwo)
-
-		//pour bucket two in bucket one
-		for bucketTwo > 0 {
-			bucketOne++
-			bucketTwo--
-		}
-		moves++
-		printer(moves, bucketOne, bucketTwo)
-
-		//fill bucket two
-		bucketTwo = sizeBucketTwo
-		moves++
-		printer(moves, bucketOne, bucketTwo)
-
-		//pour bucket two in bucket one
-		for bucketOne < sizeBucketOne {
-			bucketOne++
-			bucketTwo--
-		}
-		moves++
-		printer(moves, bucketOne, bucketTwo)
-
-		// empty bucket one
-		bucketOne = 0
-		moves++
-		printer(moves, bucketOne, bucketTwo)
-
-		//pour bucket two in bucket one
-		for bucketOne < sizeBucketOne {
-			bucketOne++
-			bucketTwo--
-		}
-		moves++
-		printer(moves, bucketOne, bucketTwo)
-
-		// return two, moves, bucketOne, nil
+	if other.level == goal {
+		this, other = other, this
 	}
-
-	return invalid()
+	return names[this.name], moves, other.level, nil
 }
 
-func printer(step, bucketOne, bucketTwo int) {
-	fmt.Printf("step: %d, bucketOne: %d, bucketTwo: %d\n\n", step, bucketOne, bucketTwo)
+func validateInput(thisSize, otherSize, goal int, start string) error {
+	if thisSize <= 0 || otherSize <= 0 || goal <= 0 ||
+		(goal > thisSize && goal > otherSize) ||
+		(start != names[1] && start != names[2]) ||
+		goal%gcd(thisSize, otherSize) != 0 {
+		return errors.New("invalid input or no solution")
+	}
+	return nil
+}
+
+func gcd(a, b int) int {
+	for ; b != 0; a, b = b, a%b {
+	}
+	return a
 }
