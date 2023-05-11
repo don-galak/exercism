402461cf782e8ab60926cdcfc7afe5c349f2280d
don-galak <ptagalac@gmail.com>
2023-05-11 10:12:25 +0300

    poker: improve

--- poker/poker.go
+++ poker/poker.go
@@ -5,15 +5,10 @@ import (
 	"strings"
 )
 
-var ranks = []string{"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"}
+var ranks = []string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
 var suites = []string{"♢", "♡", "♤", "♧"}
 
 func BestHand(hands []string) (o []string, e error) {
-
-	// if len(hands) == 1 {
-	// 	return hands, nil
-	// }
-
 	for _, hand := range hands {
 		if !isValid(hand) {
 			return nil, errors.New("invalid hand")
@@ -25,23 +20,16 @@ func BestHand(hands []string) (o []string, e error) {
 
 func isValid(hand string) bool {
 	cards := strings.Fields(hand)
-
 	if len(cards) != 5 {
 		return false
 	}
-
 	for _, card := range cards {
 		cardLen := len(card)
-
-		if cardLen < 4 || cardLen > 5 {
-			return false
-		}
-
-		if !(isFragmentValid(card[:cardLen-3], ranks) && isFragmentValid(card[cardLen-3:], suites)) {
+		if cardLen < 4 || cardLen > 5 ||
+			!(isFragmentValid(card[:cardLen-3], ranks) && isFragmentValid(card[cardLen-3:], suites)) {
 			return false
 		}
 	}
-
 	return true
 }
 
