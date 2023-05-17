5e1add4785d12e819e94f978f2d5929075f5f40a
don-galak <ptagalac@gmail.com>
2023-05-17 10:16:14 +0300

    robot-simulator: attempt at step2

--- robot-simulator/robot_simulator.go
+++ robot-simulator/robot_simulator.go
@@ -48,14 +48,51 @@ func (d Dir) String() string {
 
 // Step 2
 // Define Action type here.
-type Action int
+type Action byte
 
 func StartRobot(command chan Command, action chan Action) {
-	panic("Please implement the StartRobot function")
+	for c := range command {
+		action <- Action(c)
+	}
 }
 
 func Room(extent Rect, robot Step2Robot, action chan Action, report chan Step2Robot) {
-	panic("Please implement the Room function")
+	for a := range action {
+		switch a {
+		case 'A':
+			switch robot.Dir {
+			case N:
+				if robot.Northing+1 <= extent.Max.Northing {
+					robot.Northing++
+				}
+			case S:
+				if robot.Northing-1 >= extent.Min.Northing {
+					robot.Northing--
+				}
+			case W:
+				if robot.Easting-1 >= extent.Min.Easting {
+					robot.Easting--
+				}
+			case E:
+				if robot.Easting+1 <= extent.Max.Easting {
+					robot.Easting++
+				}
+			}
+		case 'R':
+			if robot.Dir == W {
+				robot.Dir = N
+			} else {
+				robot.Dir++
+			}
+		case 'L':
+			if robot.Dir == N {
+				robot.Dir = W
+			} else {
+				robot.Dir--
+			}
+		}
+	}
+	report <- robot
 }
 
 // Step 3
