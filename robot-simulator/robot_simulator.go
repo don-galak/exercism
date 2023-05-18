54c107cfa4aa6c33feb0a676b5edf0ce5868090e
don-galak <ptagalac@gmail.com>
2023-05-18 09:04:07 +0300

    robot-simulator: solution for TestOneStep3

--- robot-simulator/robot_simulator.go
+++ robot-simulator/robot_simulator.go
@@ -1,9 +1,5 @@
 package robot
 
-// See defs.go for other definitions
-
-// Step 1
-// Define N, E, S, W here.
 const (
 	N Dir = iota
 	E
@@ -46,8 +42,6 @@ func (d Dir) String() string {
 	return directions[d]
 }
 
-// Step 2
-// Define Action type here.
 type Action byte
 
 func StartRobot(command chan Command, action chan Action) {
@@ -97,14 +91,84 @@ func Room(extent Rect, robot Step2Robot, action chan Action, report chan Step2Ro
 	}
 }
 
-// Step 3
-// Define Action3 type here.
-type Action3 int
+type Action3 struct {
+	robotName string
+	command   rune
+}
 
 func StartRobot3(name, script string, action chan Action3, log chan string) {
-	panic("Please implement the StartRobot3 function")
+	for _, command := range script {
+		action <- Action3{name, command}
+	}
+	action <- Action3{command: 0}
 }
 
 func Room3(extent Rect, robots []Step3Robot, action chan Action3, rep chan []Step3Robot, log chan string) {
-	panic("Please implement the Room3 function")
+	for a := range action {
+		index := findRobotIndex(robots, a.robotName)
+		switch a.command {
+		case 'A':
+			switch robots[index].Dir {
+			case N:
+				if robots[index].Northing+1 <= extent.Max.Northing {
+					robots[index].Northing++
+				} else {
+					log <- "bump"
+				}
+			case S:
+				if robots[index].Northing-1 >= extent.Min.Northing {
+					robots[index].Northing--
+				} else {
+					log <- "bump"
+				}
+			case W:
+				if robots[index].Easting-1 >= extent.Min.Easting {
+					robots[index].Easting--
+				} else {
+					log <- "bump"
+				}
+			case E:
+				if robots[index].Easting+1 <= extent.Max.Easting {
+					robots[index].Easting++
+				} else {
+					log <- "bump"
+				}
+			}
+		case 'R':
+			if robots[index].Dir == W {
+				robots[index].Dir = N
+			} else {
+				robots[index].Dir++
+			}
+		case 'L':
+			if robots[index].Dir == N {
+				robots[index].Dir = W
+			} else {
+				robots[index].Dir--
+			}
+		case 0:
+			rep <- robots
+		}
+	}
+}
+
+// func advance(direction *RU, border RU, step RU, log chan string) {
+// 	switch step {
+// 	case 1:
+// 		if (*direction)+step <= border {
+// 			*direction = *direction + step
+// 			// robots[index].Northing++
+// 		} else {
+// 			log <- "bump"
+// 		}
+// 	}
+// }
+
+func findRobotIndex(robots []Step3Robot, name string) int {
+	for i := range robots {
+		if robots[i].Name == name {
+			return i
+		}
+	}
+	return -1
 }
