0c2dd02d3724458ff0b610fa5bf70b2227d5a6e1
tagkalakis <panagiotis.tagkalakis@allwyn-lotterysolutions.com>
2024-10-19 12:48:46 +0300

    elixir: add kitchen_calculator solution

--- elixir/kitchen-calculator/lib/kitchen_calculator.ex
+++ elixir/kitchen-calculator/lib/kitchen_calculator.ex
@@ -1,19 +1,19 @@
 defmodule KitchenCalculator do
   def get_volume(volume_pair), do: elem(volume_pair, 1)
 
+  def to_milliliter({:milliliter, quantity}), do: {:milliliter, quantity}
   def to_milliliter({:cup, quantity}), do: {:milliliter, quantity * 240}
   def to_milliliter({:fluid_ounce, quantity}), do: {:milliliter, quantity * 30}
   def to_milliliter({:teaspoon, quantity}), do: {:milliliter, quantity * 5}
   def to_milliliter({:tablespoon, quantity}), do: {:milliliter, quantity * 15}
-  def to_milliliter({:milliliter, quantity}), do: {:milliliter, quantity}
 
-  def from_milliliter(volume_pair, unit) when unit == :cup, do: {unit, elem(volume_pair, 1)/240}
-  def from_milliliter(volume_pair, unit) when unit == :fluid_ounce, do: {unit, elem(volume_pair, 1)/30}
-  def from_milliliter(volume_pair, unit) when unit == :teaspoon, do: {unit, elem(volume_pair, 1)/5}
-  def from_milliliter(volume_pair, unit) when unit == :tablespoon, do: {unit, elem(volume_pair, 1)/15}
-  def from_milliliter(volume_pair, unit) when unit == :milliliter, do: {unit, elem(volume_pair, 1)}
+  def from_milliliter(volume_pair, :milliliter), do: volume_pair
+  def from_milliliter(volume_pair, :cup), do: {:cup, get_volume(volume_pair) / 240}
+  def from_milliliter(volume_pair, :fluid_ounce), do: {:fluid_ounce, get_volume(volume_pair) / 30}
+  def from_milliliter(volume_pair, :teaspoon), do: {:teaspoon, get_volume(volume_pair) / 5}
+  def from_milliliter(volume_pair, :tablespoon), do: {:tablespoon, get_volume(volume_pair) / 15}
 
   def convert(volume_pair, unit) do
-    # Please implement the convert/2 function
+    from_milliliter(to_milliliter(volume_pair), unit)
   end
 end
