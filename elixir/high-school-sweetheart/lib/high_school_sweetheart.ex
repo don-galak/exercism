e7afe6004fdc871686431d016a633bee9e09776a
tagkalakis <panagiotis.tagkalakis@allwyn-lotterysolutions.com>
2024-10-21 18:32:43 +0300

    elixir: finish high school sweetheart

--- elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
+++ elixir/high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -1,32 +1,31 @@
 defmodule HighSchoolSweetheart do
-  def first_letter(name) do
-    # Please implement the first_letter/1 function
-  end
+  def first_letter(name), do: name |> String.trim() |> String.first()
 
-  def initial(name) do
-    # Please implement the initial/1 function
-  end
+  def initial(name), do: String.upcase(first_letter(name)) <> "."
 
   def initials(full_name) do
-    # Please implement the initials/1 function
+    [first, last] = String.split(full_name)
+    initial(first) <> " " <> initial(last)
   end
 
   def pair(full_name1, full_name2) do
-    #      ******       ******
-    #    **      **   **      **
-    #  **         ** **         **
-    # **            *            **
-    # **                         **
-    # **     X. X.  +  X. X.     **
-    #  **                       **
-    #    **                   **
-    #      **               **
-    #        **           **
-    #          **       **
-    #            **   **
-    #              ***
-    #               *
-
-    # Please implement the pair/2 function
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    """ <>
+      "**     #{initials(full_name1)}  +  #{initials(full_name2)}     **\n" <>
+      """
+       **                       **
+         **                   **
+           **               **
+             **           **
+               **       **
+                 **   **
+                   ***
+                    *
+      """
   end
 end
