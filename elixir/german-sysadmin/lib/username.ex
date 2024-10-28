82411c4b2d24fab7caeab52e0b8f41b2b2bb20f6
tagkalakis <panagiotis.tagkalakis@allwyn-lotterysolutions.com>
2024-10-28 23:08:04 +0200

    exlixir: username final

--- elixir/german-sysadmin/lib/username.ex
+++ elixir/german-sysadmin/lib/username.ex
@@ -1,10 +1,17 @@
 defmodule Username do
-  def sanitize(username) do
-    # ä becomes ae
-    # ö becomes oe
-    # ü becomes ue
-    # ß becomes ss
+  def sanitize([]), do: []
 
-    # Please implement the sanitize/1 function
+  def sanitize([head | tail]) do
+    username =
+    case head do
+      ?ä -> ~c"ae"
+      ?ö -> ~c"oe"
+      ?ü -> ~c"ue"
+      ?ß -> ~c"ss"
+      ?_ -> ~c"_"
+      head when head >= ?a and head <= ?z -> [head]
+      _ -> []
+    end
+    username ++ sanitize(tail)
   end
 end
