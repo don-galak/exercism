ec2dea722adcf25b247a4db644052728d318e4b7
tagkalakis <panagiotis.tagkalakis@allwyn-lotterysolutions.com>
2024-11-09 21:05:50 +0200

    exlixir: library fees final

--- elixir/21.library-fees/lib/library_fees.ex
+++ elixir/21.library-fees/lib/library_fees.ex
@@ -1,25 +1,43 @@
 defmodule LibraryFees do
   def datetime_from_string(string) do
-    # Please implement the datetime_from_string/1 function
+    NaiveDateTime.from_iso8601!(string)
   end
 
   def before_noon?(datetime) do
-    # Please implement the before_noon?/1 function
+    datetime.hour < 12
   end
 
   def return_date(checkout_datetime) do
-    # Please implement the return_date/1 function
+    if before_noon?(checkout_datetime) do
+      NaiveDateTime.to_date(NaiveDateTime.add(checkout_datetime, 28, :day))
+    else
+      NaiveDateTime.to_date(NaiveDateTime.add(checkout_datetime, 29, :day))
+    end
   end
 
   def days_late(planned_return_date, actual_return_datetime) do
-    # Please implement the days_late/2 function
+    diff = Date.diff(actual_return_datetime, planned_return_date)
+    if diff < 0 do
+      0
+    else
+      diff
+    end
   end
 
   def monday?(datetime) do
-    # Please implement the monday?/1 function
+    Date.day_of_week(NaiveDateTime.to_date(datetime)) == 1
   end
 
   def calculate_late_fee(checkout, return, rate) do
-    # Please implement the calculate_late_fee/3 function
+    actual_return_datetime = datetime_from_string(return)
+    checkout_date = return_date(datetime_from_string(checkout))
+
+    fee = days_late(checkout_date, actual_return_datetime) * rate
+
+    if monday?(actual_return_datetime) do
+      trunc(fee / 2)
+    else
+      fee
+    end
   end
 end
