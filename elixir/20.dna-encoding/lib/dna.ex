8e1ffde7f982fc47b086ecc5a074199f0189bb12
tagkalakis <panagiotis.tagkalakis@allwyn-lotterysolutions.com>
2024-11-09 19:52:14 +0200

    exlixir: first try

--- elixir/20.dna-encoding/lib/dna.ex
+++ elixir/20.dna-encoding/lib/dna.ex
@@ -1,15 +1,21 @@
 defmodule DNA do
-  def encode_nucleotide(code_point) do
-    # Please implement the encode_nucleotide/1 function
-  end
+  def encode_nucleotide(?\s), do: 0
+  def encode_nucleotide(?A), do: 1
+  def encode_nucleotide(?C), do: 2
+  def encode_nucleotide(?G), do: 4
+  def encode_nucleotide(?T), do: 8
 
-  def decode_nucleotide(encoded_code) do
-    # Please implement the decode_nucleotide/1 function
-  end
+  def decode_nucleotide(0), do: ?\s
+  def decode_nucleotide(1), do: ?A
+  def decode_nucleotide(2), do: ?C
+  def decode_nucleotide(4), do: ?G
+  def decode_nucleotide(8), do: ?T
 
-  def encode(dna) do
-    # Please implement the encode/1 function
-  end
+  def encode(dna), do: do_encode(dna, "")
+
+  defp do_encode([], acc), do: acc
+  defp do_encode([head | tail], acc), do: do_encode(tail, "")
+
 
   def decode(dna) do
     # Please implement the decode/1 function
