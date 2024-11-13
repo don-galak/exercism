d564c47a94d9e229020a9c50a3d8cc8d6d3fb488
tagkalakis <panagiotis.tagkalakis@allwyn-lotterysolutions.com>
2024-11-13 23:27:51 +0200

    exlixir: finalize boutique

--- elixir/23.boutique-inventory/lib/boutique_inventory.ex
+++ elixir/23.boutique-inventory/lib/boutique_inventory.ex
@@ -1,19 +1,20 @@
 defmodule BoutiqueInventory do
-  def sort_by_price(inventory), do: Enum.sort(inventory, fn a, b -> a.price <= b.price end)
+  def sort_by_price(inventory), do: Enum.sort_by(inventory, & &1.price)
 
   def with_missing_price(inventory), do: Enum.filter(inventory, fn item -> item.price == nil end)
 
   def update_names(inventory, old_word, new_word) do
-    Enum.map(inventory, fn %{name: name}=item ->
+    Enum.map(inventory, fn %{name: name} = item ->
       %{item | name: String.replace(name, old_word, new_word)}
     end)
   end
 
-  def increase_quantity(item, count) do
-    # Please implement the increase_quantity/2 function
-  end
+  def increase_quantity(item, count),
+    do: %{
+      item
+      | quantity_by_size: Map.new(item.quantity_by_size, fn {key, val} -> {key, val + count} end)
+    }
 
-  def total_quantity(item) do
-    # Please implement the total_quantity/1 function
-  end
+  def total_quantity(item),
+    do: Enum.reduce(item.quantity_by_size, 0, fn {_, val}, acc -> acc + val end)
 end
