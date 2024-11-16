1d38109e974f2e4ce8c36dddc2e6afbe91022fe0
tagkalakis <panagiotis.tagkalakis@allwyn-lotterysolutions.com>
2024-11-16 13:45:19 +0200

    exlixir: newsletter final

--- elixir/25.newsletter/lib/newsletter.ex
+++ elixir/25.newsletter/lib/newsletter.ex
@@ -1,19 +1,21 @@
 defmodule Newsletter do
-  def read_emails(path) do
-    {_, contents} = File.read(path)
-    if String.length(contents) > 0, do: String.split(String.trim(contents), "\n"), else: []
-  end
+  @spec read_emails(path :: String.t()) :: list(String.t())
+  def read_emails(path), do: String.split(File.read!(path))
 
-  def open_log(path) do
-    {_, pid} = File.open(path, [:write])
-    pid
-  end
+  def open_log(path), do: File.open!(path, [:write])
 
   def log_sent_email(pid, email), do: IO.puts(pid, email)
 
   def close_log(pid), do: File.close(pid)
 
+  @spec send_newsletter(String.t(), pid(), (String.t() -> any())) :: any
   def send_newsletter(emails_path, log_path, send_fun) do
-    # Please implement the send_newsletter/3 function
+    pid = open_log(log_path)
+
+    Enum.each(read_emails(emails_path), fn email ->
+      if send_fun.(email) == :ok, do: log_sent_email(pid, email)
+    end)
+
+    close_log(pid)
   end
 end
