c7eaf17a81e84be5803783526883c16ee4db09af
tagkalakis <panagiotis.tagkalakis@allwyn-lotterysolutions.com>
2024-12-02 23:55:34 +0200

    exlixir: captains log final

--- elixir/31.captains-log/lib/captains_log.ex
+++ elixir/31.captains-log/lib/captains_log.ex
@@ -4,7 +4,7 @@ defmodule CaptainsLog do
   @min_range 42000
 
   def random_planet_class() do
-    Enum.at(@planetary_classes, Enum.random(0..9))
+    Enum.random(@planetary_classes)
   end
 
   def random_ship_registry_number() do
@@ -16,6 +16,6 @@ defmodule CaptainsLog do
   end
 
   def format_stardate(stardate) do
-    # Please implement the format_stardate/1 function
+    to_string(:io_lib.format("~.1f", [stardate]))
   end
 end
